5fc6f14b0fbc9fe5db2b9584298d8a826f94e155
ZeroC <git@zeroc.com>
2025-02-24 02:27:00 +0000

    ice: 3.8.0-nightly.20250224.1

---
+++ Formula/ice.rb
@@ -0,0 +1,67 @@
+class Ice < Formula
+    desc "Comprehensive RPC framework"
+    homepage "https://zeroc.com"
+
+    version "3.8.0-nightly.20250224.1"
+    url "https://github.com/zeroc-ice/ice/archive/8409afdf7a2d9554636020f66ff07d779daf8e20.tar.gz"
+    sha256 "c69f0a1432b692b2a5cce6da354a03b66871020d127591bef1b46e01eb198885"
+
+    depends_on "lmdb"
+    depends_on "mcpp"
+
+    def install
+      args = [
+        "prefix=#{prefix}",
+        "V=1",
+        "USR_DIR_INSTALL=yes", # ensure slice and man files are installed to share
+        "MCPP_HOME=#{Formula["mcpp"].opt_prefix}",
+        "LMDB_HOME=#{Formula["lmdb"].opt_prefix}",
+        "CONFIGS=all",
+        "PLATFORMS=all",
+        "LANGUAGES=cpp",
+      ]
+      system "make", "install", *args
+
+      (libexec/"bin").mkpath
+      %w[slice2py slice2rb slice2js].each do |r|
+        mv bin/r, libexec/"bin"
+      end
+    end
+
+    test do
+      (testpath / "Hello.ice").write <<~EOS
+        module Test
+        {
+            interface Hello
+            {
+                void sayHello();
+            }
+        }
+      EOS
+      (testpath / "Test.cpp").write <<~EOS
+        #include "Hello.h"
+        #include <Ice/Ice.h>
+
+        class HelloI : public Test::Hello
+        {
+        public:
+            void sayHello(const Ice::Current&) override {}
+        };
+
+        int main(int argc, char* argv[])
+        {
+          Ice::CommunicatorHolder ich(argc, argv);
+          auto adapter = ich->createObjectAdapterWithEndpoints("Hello", "default -h localhost");
+          adapter->add(std::make_shared<HelloI>(), Ice::stringToIdentity("hello"));
+          adapter->activate();
+          return 0;
+        }
+      EOS
+
+      system "#{bin}/slice2cpp", "Hello.ice"
+      system "xcrun", "clang++", "-std=c++20", "-c", "-I#{include}", "Hello.cpp"
+      system "xcrun", "clang++", "-std=c++20", "-c", "-I#{include}", "Test.cpp"
+      system "xcrun", "clang++", "-L#{lib}", "-o", "test", "Test.o", "Hello.o", "-lIce", "-lpthread"
+      system "./test"
+    end
+  end
