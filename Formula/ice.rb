3f5feab351c314674d025e2bce3e2ddfe1856f14
ZeroC <git@zeroc.com>
2025-05-11 02:36:57 +0000

    ice: add 3.8.0-nightly.20250511.1 bottle.

--- Formula/ice.rb
+++ Formula/ice.rb
@@ -6,6 +6,11 @@ class Ice < Formula
     url "https://github.com/zeroc-ice/ice/archive/b2e8aaaffd8ac672d400f6470f72dc759556e40f.tar.gz"
     sha256 "a48b84d8288302f2b482a8abc686df6a5d42c0e071d1327c3c98154b10ad7235"
 
+  bottle do
+    root_url "https://download.zeroc.com/nexus/repository/nightly"
+    sha256 cellar: :any, arm64_sonoma: "7a9ccae65988e566f7d80788618c2a1a184c8d55c5c18936fb3e6f7e7d81bd87"
+  end
+
     depends_on "lmdb"
     depends_on "mcpp"
 
