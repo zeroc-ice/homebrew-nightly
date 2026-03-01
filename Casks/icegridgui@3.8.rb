83097b450614a8c3fd8de948c591d9cde2ae368a
ZeroC <git@zeroc.com>
2026-03-01 04:10:09 +0000

    ice@3.8, icegridgui@3.8: 3.8.2-nightly.20260301.1

--- Casks/icegridgui@3.8.rb
+++ Casks/icegridgui@3.8.rb
@@ -1,8 +1,8 @@
 cask "icegridgui@3.8" do
-  version "3.8.2-nightly.20260228.1"
-  sha256 "7cb111ddf79adaa67b7eecc5b20b6ba0b6b9dbef8a46d58287565c94aae3dcd1"
+  version "3.8.2-nightly.20260301.1"
+  sha256 "c6b07d8b0ce7e719dfda5d3fa3d5eca4f362583059b5e74dd2de8e9ed3990837"
 
-  url "https://download.zeroc.com/ice/nightly/3.8/IceGridGUI-3.8.2-nightly.20260228.1.dmg"
+  url "https://download.zeroc.com/ice/nightly/3.8/IceGridGUI-3.8.2-nightly.20260301.1.dmg"
   name "IceGrid GUI"
   desc "Graphical administration tool for IceGrid"
   homepage "https://zeroc.com"
