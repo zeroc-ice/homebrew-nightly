624bb30ef599e86bb7ae2dde78c4b624e073918d
ZeroC <git@zeroc.com>
2026-09-24 02:38:04 +0000

    ice@3.9, icegridgui@3.9: 3.9.0-nightly.20260924.1

--- Casks/icegridgui@3.9.rb
+++ Casks/icegridgui@3.9.rb
@@ -1,8 +1,8 @@
 cask "icegridgui@3.9" do
-  version "3.9.0-nightly.20260923.1"
-  sha256 "43299ac28dc8e24945aabe8bb1fa3c79fb4a0bb53f5bae93d5a4fb40c23a1b7e"
+  version "3.9.0-nightly.20260924.1"
+  sha256 "32a04b77ef72acbbe10b545363d50acf8f716c0aa2106db5fb1b6566e4a43b49"
 
-  url "https://download.zeroc.com/ice/nightly/3.9/IceGridGUI-3.9.0-nightly.20260923.1.dmg"
+  url "https://download.zeroc.com/ice/nightly/3.9/IceGridGUI-3.9.0-nightly.20260924.1.dmg"
   name "IceGrid GUI"
   desc "Graphical administration tool for IceGrid"
   homepage "https://zeroc.com"
--- Formula/ice@3.9.rb
+++ Formula/ice@3.9.rb
@@ -2,13 +2,13 @@ class IceAT39 < Formula
     desc "Comprehensive RPC framework"
     homepage "https://zeroc.com"
 
-    version "3.9.0-nightly.20260923.1"
+    version "3.9.0-nightly.20260924.1"
     url "https://github.com/zeroc-ice/ice/archive/4a31269cb3a008b6561d8a9b8e17a5efd4516300.tar.gz"
     sha256 "a3f36feeefec0d2568a86d0eb8a1e7f35f89224d0f2f3910ecd1bbf1442b5176"
 
   bottle do
     root_url "https://download.zeroc.com/ice/nightly/3.9"
-    sha256 cellar: :any, arm64_tahoe: "72b425e99a2aabf628062b26dcc37ce6df028c4bde29c92b9797e9c662465272"
+    sha256 cellar: :any, arm64_tahoe: "68e47723b40b80ad6465419a729fd351aa31e34bbabaa72eefa77d0bd016229e"
   end
 
     depends_on "lmdb"
